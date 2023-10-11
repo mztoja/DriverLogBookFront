619ed1506ef9b32b34b6532674f07b8538329513
mztoja <mztoja@gmail.com>
2023-10-11 20:04:39 +0200

    Added add place form

---
+++ src/components/common/form/TextArea.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {TextField} from "@mui/material";
+import './Form.css';
+
+export interface Props {
+    label: string;
+    value:string;
+    onChange: (e:any) => void;
+}
+
+
+export const TextArea = (props:Props) => {
+
+    return (
+        <TextField
+            id={props.label}
+            label={props.label}
+            InputLabelProps={{className: 'TextInput__Label'}}
+            InputProps={{className: 'TextInput'}}
+            multiline
+            fullWidth
+            value={props.value}
+            onChange={props.onChange}
+            maxRows={8}
+        />
+    );
+}
+
+
+
+
+
+
+
+
---
+++ src/components/common/form/place/PlaceTypeSelect.tsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import {MenuItem, Select} from "@mui/material";
+import { form } from "../../../../assets/txt/form";
+import {InputPropsTypes} from "../../../../types/InputPropsTypes";
+
+export const PlaceTypeSelect = (props: InputPropsTypes) => {
+
+    const [value, setValue] = useState<number>(Number(props.value));
+
+    useEffect(() => {
+        props.onChange(value);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[value]);
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="placeType" className="TextInput__Label">{form[props.lang].placeType}</InputLabel>
+            <Select
+                label={form[props.lang].placeType}
+                id="placeType"
+                value={props.value ? props.value : '0'}
+                onChange={(e) => {setValue(Number(e.target.value));}}
+                inputProps={{className: 'TextInput'}}
+            >
+                <MenuItem value={0}>{form[props.lang].placeType0}</MenuItem>
+                <MenuItem value={1}>{form[props.lang].placeType1}</MenuItem>
+                <MenuItem value={2}>{form[props.lang].placeType2}</MenuItem>
+                <MenuItem value={3}>{form[props.lang].placeType3}</MenuItem>
+                <MenuItem value={4}>{form[props.lang].placeType4}</MenuItem>
+                <MenuItem value={5}>{form[props.lang].placeType5}</MenuItem>
+                <MenuItem value={6}>{form[props.lang].placeType6}</MenuItem>
+                <MenuItem value={7}>{form[props.lang].placeType7}</MenuItem>
+            </Select>
+        </FormControl>);
+}
