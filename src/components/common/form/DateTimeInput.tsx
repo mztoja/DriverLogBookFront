37e17ead9814bce851aefb08bc234d3fba9edba6
mztoja <mztoja@gmail.com>
2023-12-28 20:20:12 +0100

    Added vehicle adding functionality

---
+++ src/components/common/form/DateTimeInput.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import {TextField} from "@mui/material";
+import {form} from "../../../assets/txt/form";
+import './Form.css';
+import {InputPropsTypes} from "../../../types/InputPropsTypes";
+
+export const DateTimeInput = (props: InputPropsTypes) => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date(new Date().getTime() + 60*60*1000).toISOString().slice(0, 16));
+
+    const handleDateChange = (e: any) => {
+        setSelectedDate(e.target.value);
+    };
+
+    useEffect(() => {
+        props.onChange(selectedDate);
+    },[selectedDate, props]);
+
+    return (
+        <TextField
+            label={form[props.lang].date}
+            id="datetime"
+            InputLabelProps={{className: 'TextInput__Label'}}
+            InputProps={{className: 'TextInput'}}
+            type="datetime-local"
+            value={selectedDate}
+            onChange={handleDateChange}
+            fullWidth
+            autoComplete="off"
+            size='small'
+        />
+    );
+}
