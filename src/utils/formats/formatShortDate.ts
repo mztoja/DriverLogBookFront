0c476378284c2fedafe5ba7b5af25bfaeab0b84f
mztoja <mztoja@gmail.com>
2024-07-10 20:47:46 +0200

    date show repair

--- src/utils/formats/formatShortDate.ts
+++ src/utils/formats/formatShortDate.ts
@@ -1,7 +1,7 @@
 export const formatShortDate = (dateString: string): string => {
     const date = new Date(dateString);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear().toString();
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    const year = date.getUTCFullYear().toString();
     return `${day}.${month}.${year}`;
 }
